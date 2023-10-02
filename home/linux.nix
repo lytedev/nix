c584b63e3e9a70d81d75154239819a1e6b5ce69b
Daniel Flanagan <daniel@lyte.dev>
2023-10-02 11:29:13 -0500

    Fix weird spacing

--- home/linux.nix
+++ home/linux.nix
@@ -661,14 +661,10 @@
           };
 
           extraConfig = ''
-            user_pref("
-            toolkit.legacyUserProfileCustomizations.stylesheets ", true);
-            // user_pref("
-            full-screen-api.ignore-widgets ", true);
-            user_pref("
-            media.ffmpeg.vaapi.enabled ", true);
-            user_pref("
-            media.rdd-vpx.enabled ", true);
+            user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+            // user_pref("full-screen-api.ignore-widgets", true);
+            user_pref("media.ffmpeg.vaapi.enabled", true);
+            user_pref("media.rdd-vpx.enabled", true);
           '';
 
           userChrome = ''
