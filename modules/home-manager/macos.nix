a758ff909ffd5af9cdd1e72790780de714278dec
Daniel Flanagan <daniel@lyte.dev>
2023-10-06 02:05:32 -0500

    Note on pinentry

--- modules/home-manager/macos.nix
+++ modules/home-manager/macos.nix
@@ -3,4 +3,13 @@
     common
     desktop
   ];
+
+  # TODO: pinentry curses?
+  /*
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryFlavor = "gnome3";
+    enableSSHSupport = true;
+  };
+  */
 }
