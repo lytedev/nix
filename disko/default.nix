507c4e53d0ff569138b69ffd1fa7a383fcb053e5
Daniel Flanagan <daniel@lyte.dev>
2023-10-06 10:42:19 -0500

    Add legacy disk partitioning scheme

--- disko/default.nix
+++ disko/default.nix
@@ -172,4 +172,43 @@
       };
     };
   };
+  legacy = {disks ? ["/dev/vda"], ...}: {
+    disko.devices = {
+      disk = {
+        primary = {
+          device = builtins.elemAt disks 0;
+          type = "disk";
+          content = {
+            type = "table";
+            format = "gpt";
+            partitions = [
+              {
+                name = "ESP";
+                start = "1M";
+                end = "500M";
+                bootable = true;
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+              }
+              {
+                name = "root";
+                start = "500M";
+                end = "100%";
+                part-type = "primary";
+                bootable = true;
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              }
+            ];
+          };
+        };
+      };
+    };
+  };
 }
