2ead8292dac7719f6d62134921a509dcd3f3e48e
Daniel Flanagan <daniel@lyte.dev>
2023-11-21 23:42:42 -0600

    Fix last warning

--- modules/home-manager/bat.nix
+++ modules/home-manager/bat.nix
@@ -6,18 +6,18 @@
   programs.bat = {
     enable = true;
     config = {
-      theme = "Catppuccin-mocha";
+      theme = "ansi";
     };
-    themes = {
-      "Catppuccin-mocha" = builtins.readFile (pkgs.fetchFromGitHub
-        {
-          owner = "catppuccin";
-          repo = "bat";
-          rev = "477622171ec0529505b0ca3cada68fc9433648c6";
-          sha256 = "6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-        }
-        + "/Catppuccin-mocha.tmTheme");
-    };
+    # themes = {
+    #   "Catppuccin-mocha" = builtins.readFile (pkgs.fetchFromGitHub
+    #     {
+    #       owner = "catppuccin";
+    #       repo = "bat";
+    #       rev = "477622171ec0529505b0ca3cada68fc9433648c6";
+    #       sha256 = "6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+    #     }
+    #     + "/Catppuccin-mocha.tmTheme");
+    # };
   };
 
   home.shellAliases = {
