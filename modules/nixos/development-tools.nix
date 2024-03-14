bde0ce2e51c26908fe1c6d9ef0c34ae4cfc9b9bb
Daniel Flanagan <daniel@lyte.dev>
2024-03-14 14:04:31 -0500

    Moat tools

--- modules/nixos/development-tools.nix
+++ modules/nixos/development-tools.nix
@@ -2,12 +2,15 @@
   environment.systemPackages = with pkgs; [
     watchexec
     android-tools
+    kubectl
+    stern
     libresprite
     audacity
     wol
     shellcheck
     shfmt
     nodePackages.bash-language-server
+    nodePackages.yaml-language-server
     xh
     jq
     curl
