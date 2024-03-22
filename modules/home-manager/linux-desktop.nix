85efa83e66f2d986ca062bbf4c83117727482ab0
Daniel Flanagan <daniel@lyte.dev>
2024-03-22 10:05:36 -0500

    Currently, catppuccin-gtk fails to build, so simply avoid using it for now
    https://github.com/NixOS/nixpkgs/issues/298043

--- modules/home-manager/linux-desktop.nix
+++ modules/home-manager/linux-desktop.nix
@@ -12,15 +12,16 @@
 
   gtk = {
     enable = true;
-    theme = {
-      name = "Catppuccin-Mocha-Compact-Sapphire-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = ["sapphire"];
-        size = "compact";
-        tweaks = ["rimless"];
-        variant = "mocha";
-      };
-    };
+    # Currently failing: https://github.com/NixOS/nixpkgs/issues/298043
+    # theme = {
+    #   name = "Catppuccin-Mocha-Compact-Sapphire-Dark";
+    #   package = pkgs.catppuccin-gtk.override {
+    #     accents = ["sapphire"];
+    #     size = "compact";
+    #     tweaks = ["rimless"];
+    #     variant = "mocha";
+    #   };
+    # };
   };
 
   home.pointerCursor = {
