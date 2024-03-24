8c4a196883d52dd0d3ba9a4e343cf387b61b832d
Daniel Flanagan <daniel@lyte.dev>
2024-03-24 18:02:59 -0500

    Add gitui theme

--- modules/nixos/development-tools.nix
+++ modules/nixos/development-tools.nix
@@ -8,18 +8,6 @@
   };
 
   environment.systemPackages = with pkgs; [
-    # (gitui.overrideAttrs {
-    #   version = "5b3e2c9ae3913855f5dbe463c5ae1c04430e7532";
-
-    #   src = fetchFromGitHub {
-    #     owner = "extrawurst";
-    #     repo = "gitui";
-    #     rev = "5b3e2c9ae3913855f5dbe463c5ae1c04430e7532";
-    #     hash = "sha256-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC=";
-    #   };
-
-    #   cargoHash = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=";
-    # })
     taplo # toml language server for editing helix configs per repo
     oil
     nushell
