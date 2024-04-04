3a3406c796e576ef39974e06ef57eed522aadba5
Daniel Flanagan <daniel@lyte.dev>
2024-04-04 09:24:03 -0500

    thinker: use tlp

--- nixos/thinker.nix
+++ nixos/thinker.nix
@@ -48,6 +48,10 @@
     kernelParams = ["boot.shell_on_fail" "resume_offset=22816000"];
     initrd.availableKernelModules = ["xhci_pci" "nvme" "ahci"];
   };
+  services.tlp = {
+    enable = true;
+  };
+  services.power-profiles-daemon.enable = false;
   hardware.bluetooth.enable = true;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   services.printing.enable = true; # I own a printer in the year of our Lord 2023
@@ -80,16 +84,16 @@
   # findmnt -no UUID -T /swap/swapfile
   boot.resumeDevice = "/dev/disk/by-uuid/aacd6814-a5a2-457a-bf65-8d970cb1f03d";
 
-  services.logind = {
-    lidSwitch = "suspend-then-hibernate";
-    extraConfig = ''
-      HandlePowerKey=suspend-then-hibernate
-      IdleAction=suspend-then-hibernate
-      IdleActionSec=10m
-      HandleLidSwitchDocked=ignore
-    '';
-  };
-  systemd.sleep.extraConfig = "HibernateDelaySec=30m";
+  # services.logind = {
+  #   lidSwitch = "suspend-then-hibernate";
+  #   extraConfig = ''
+  #     HandlePowerKey=suspend-then-hibernate
+  #     IdleAction=suspend-then-hibernate
+  #     IdleActionSec=10m
+  #     HandleLidSwitchDocked=ignore
+  #   '';
+  # };
+  # systemd.sleep.extraConfig = "HibernateDelaySec=30m";
 
   networking = {
     firewall = {
