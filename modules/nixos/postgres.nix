18c2274529e60fde16e394a1bb66397ee01d2caf
Daniel Flanagan <daniel@lyte.dev>
2024-05-23 09:41:15 -0500

    chore: update flake

--- modules/nixos/postgres.nix
+++ modules/nixos/postgres.nix
@@ -29,11 +29,11 @@
 
     identMap = ''
       # ArbitraryMapName systemUser DBUser
-        superuser_map    root       postgres
-        superuser_map    postgres   postgres
-        superuser_map    daniel     postgres
-        # Let other names login as themselves
-        superuser_map   /^(.*)$    \1
+      superuser_map    root       postgres
+      superuser_map    postgres   postgres
+      superuser_map    daniel     postgres
+      # Let other names login as themselves
+      superuser_map   /^(.*)$    \1
     '';
   };
 
