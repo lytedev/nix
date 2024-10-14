8afad6f40c192b422193ea18e7a99d6992112517
Daniel Flanagan <daniel@lyte.dev>
2024-10-14 09:54:39 -0500

    Add factorio server in preparation for 2.0 release

--- nixos/beefcake.nix
+++ nixos/beefcake.nix
@@ -1804,6 +1804,29 @@ sudo nix run nixpkgs#ipmitool -- raw 0x30 0x30 0x02 0xff 0x00
         extraConfig = ''reverse_proxy :5006'';
       };
     }
+    {
+      services.factorio = {
+        enable = true;
+        package = pkgs.factorio-headless.override {
+          versionsJson = ./factorio-versions.json;
+        };
+        admins = ["lytedev"];
+        autosave-interval = 5;
+        game-name = "Flanwheel Online";
+        description = "Space Age 2.0";
+        openFirewall = true;
+        # public = true; # NOTE: cannot be true if requireUserVerification is false
+        port = 34197;
+        requireUserVerification = false; # critical for DRM-free users
+        lan = true; # NOTE: not sure if this interferes with online-ability?
+
+        # contains the game password and account password for "public" servers
+        extraSettingsFile = config.sops.secrets.factorio-server-settings.path;
+      };
+      sops.secrets = {
+        factorio-server-settings = {mode = "0777";};
+      };
+    }
   ];
 
   /*
