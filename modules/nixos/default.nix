6e4d4cfafcefe4ee91bb7e46ff899bbb1191c7b6
Daniel Flanagan <daniel@lyte.dev>
2024-12-02 20:09:05 -0600

    Re-activate the cache

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -409,7 +409,7 @@
           "https://cache.nixos.org/"
           "https://helix.cachix.org"
           "https://nix-community.cachix.org"
-          # "https://nix.h.lyte.dev"
+          "https://nix.h.lyte.dev"
           "https://hyprland.cachix.org"
         ];
         trusted-public-keys = [
