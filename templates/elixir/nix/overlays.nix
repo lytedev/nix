ccad1cefd5cfdd6ba3f32d232a083becd543c18a
Daniel Flanagan <daniel@lyte.dev>
2024-12-23 18:58:37 -0600

    Fix elixir version

--- templates/elixir/nix/overlays.nix
+++ templates/elixir/nix/overlays.nix
@@ -1,7 +1,7 @@
 {
   forSelf = final: prev: {
     erlang = prev.beam.packagesWith prev.beam.interpreters.erlang_27;
-    elixir = final.erlang.elixir_1_18;
+    elixir = final.erlang.elixir_1_17;
     mixRelease = final.erlang.mixRelease.override {elixir = final.elixir;};
     fetchMixDeps = final.erlang.fetchMixDeps.override {elixir = final.elixir;};
     elixir-ls = prev.elixir-ls.override {elixir = final.elixir;};
