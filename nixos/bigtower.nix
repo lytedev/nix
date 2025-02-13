ec715b4d02a1504d40fb50a87d276146119bf22b
Daniel Flanagan <daniel@lyte.dev>
2025-02-12 20:55:11 -0600

    DHCPlease

--- nixos/bigtower.nix
+++ nixos/bigtower.nix
@@ -4,12 +4,12 @@
   config,
   ...
 }: {
+  system.stateVersion = "24.05";
+  home-manager.users.daniel.home.stateVersion = "24.05";
+  networking.hostName = "bigtower";
+  networking.useDHCP = true;
+
   imports = [
-    {
-      system.stateVersion = "24.05";
-      home-manager.users.daniel.home.stateVersion = "24.05";
-      networking.hostName = "bigtower";
-    }
     {
       fileSystems."/" = {
         device = "/dev/disk/by-uuid/5b6c2d65-2704-4ed1-b06d-5ee7110b3d28";
@@ -34,10 +34,7 @@
     }
   ];
   hardware.graphics.extraPackages = [
-    # pkgs.rocmPackages.clr.icd
     pkgs.amdvlk
-
-    # encoding/decoding acceleration
     pkgs.libvdpau-va-gl
     pkgs.vaapiVdpau
   ];
@@ -60,100 +57,5 @@
         MultiProfile = "multiple";
       };
     };
-  };
-
-  /*
-  networking = {
-    firewall = let
-      terraria = 7777;
-      stardew-valley = 24642;
-      web-dev-lan = 18888;
-      ports = [
-        terraria
-        stardew-valley
-        web-dev-lan
-      ];
-    in {
-      allowedTCPPorts = ports;
-      allowedUDPPorts = ports;
-    };
-  };
-  */
-
-  environment.systemPackages = with pkgs; [
-    radeontop
-    # godot_4
-    prismlauncher
-  ];
-
-  home-manager.users.daniel = {
-    /*
-    slippi-launcher = {
-      enable = true;
-      # isoPath = "${config.home-manager.users.daniel.home.homeDirectory}/../games/roms/dolphin/melee.iso";
-      launchMeleeOnPlay = false;
-    };
-    */
-
-    # TODO: monitor config module?
-    /*
-      wayland.windowManager.hyprland = {
-      settings = {
-        env = [
-          "EWW_BAR_MON,1"
-        ];
-        # See https://wiki.hyprland.org/Configuring/Keywords/ for more
-        monitor = [
-          # "DP-2,3840x2160@60,-2160x0,1,transform,3"
-          "DP-3,3840x2160@120,${toString (builtins.ceil (2160 / 1.5))}x0,1"
-          ## HDR breaks screenshare? "DP-3,3840x2160@120,${toString (builtins.ceil (2160 / 1.5))}x0,1,bitdepth,10"
-          ## "desc:LG Display 0x0521,3840x2160@120,0x0,1"
-          ## "desc:Dell Inc. DELL U2720Q D3TM623,3840x2160@60,3840x0,1.5,transform,1"
-          "DP-2,3840x2160@60,0x0,1.5,transform,1"
-        ];
-        input = {
-          force_no_accel = true;
-          sensitivity = 1; # -1.0 - 1.0, 0 means no modification.
-        };
-      };
-    };
-
-    wayland.windowManager.sway = {
-      config = {
-        output = {
-          "GIGA-BYTE TECHNOLOGY CO., LTD. AORUS FO48U 23070B000307" = {
-            mode = "3840x2160@120Hz";
-            position = "${toString (builtins.ceil (2160 / 1.5))},0";
-          };
-
-          "Dell Inc. DELL U2720Q D3TM623" = {
-            # desktop left vertical monitor
-            mode = "3840x2160@60Hz";
-            transform = "90";
-            scale = "1.5";
-            position = "0,0";
-          };
-        };
-
-        workspaceOutputAssign =
-          (
-            map
-            (ws: {
-              output = "GIGA-BYTE TECHNOLOGY CO., LTD. AORUS FO48U 23070B000307";
-              workspace = toString ws;
-            })
-            (lib.range 1 7)
-          )
-          ++ (
-            map
-            (ws: {
-              output = "Dell Inc. DELL U2720Q D3TM623";
-              workspace = toString ws;
-            })
-            (lib.range 8 9)
-          );
-      };
-    };
-    */
   };
 }
