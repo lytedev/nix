d75d8cbfb6c8fb5ed8aee5142a75df09bf43e7b6
Daniel Flanagan <daniel@lyte.dev>
2025-02-20 12:31:30 -0600

    feat: add installer cancellation step

--- packages/installer.nix
+++ packages/installer.nix
@@ -24,6 +24,10 @@ pkgs.writeShellApplication {
 
     echo "$pass1" | tr -d "\n" > /tmp/secret.key
 
+    echo "This will install the host configuration for '$nixos_host' to '$disk_path' using partition scheme '$partition_scheme'. All data on the disk will be lost."
+    echo "Press enter to proceed. Press Ctrl-C to cancel."
+    read -r
+
     nix-shell --packages git --run "sudo nix run \
       --extra-experimental-features nix-command \
       --extra-experimental-features flakes \
