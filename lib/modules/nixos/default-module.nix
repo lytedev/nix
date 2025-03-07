2bd95afe60aacea4e43f5eb4e926a56442b6dcd9
Daniel Flanagan <daniel@lyte.dev>
2025-03-07 16:43:52 -0600

    fix: trust the cache

--- lib/modules/nixos/default-module.nix
+++ lib/modules/nixos/default-module.nix
@@ -111,6 +111,16 @@
           "flakes"
         ];
         auto-optimise-store = lib.mkDefault true;
+        extra-substituters = [
+          "https://cache.nixos.org/"
+          "https://nix-community.cachix.org"
+          "https://nix.h.lyte.dev"
+        ];
+        extra-trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "h.lyte.dev-2:te9xK/GcWPA/5aXav8+e5RHImKYMug8hIIbhHsKPN0M="
+        ];
       };
     };
 
