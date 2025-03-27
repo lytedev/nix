1102c7028dd2ab2cf98133ab3c241758884c7f93
Daniel Flanagan <daniel@lyte.dev>
2025-03-27 14:48:52 -0500

    fix: thinker jujutsu

--- packages/hosts/thinker.nix
+++ packages/hosts/thinker.nix
@@ -26,7 +26,10 @@
   networking.wifi.enable = true;
   lyte.desktop.enable = true;
   home-manager.users.daniel = {
-    lyte.shell.enable = true;
+    lyte.shell = {
+      enable = true;
+      learn-jujutsu-not-git.enable = true;
+    };
     lyte.desktop.enable = true;
     home = {
       stateVersion = "24.11";
