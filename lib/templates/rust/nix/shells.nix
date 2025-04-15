e3b28acbb8a07d02ae56d47a2f04567c2b8a45c1
Daniel Flanagan <daniel@lyte.dev>
2025-04-15 16:17:57 -0500

    fix: add taplo to rust template, remove ghostty notifications for now

--- lib/templates/rust/nix/shells.nix
+++ lib/templates/rust/nix/shells.nix
@@ -11,13 +11,14 @@ in
     inherit (self.checks.${system}.git-hooks) shellHook;
     inputsFrom = [ self.packages.${system}.default ];
     packages = with pkgs; [
-      convco
-      rustPackages.clippy
-      typescript-language-server
-      rust-analyzer
-      rustfmt
-      nixd
-      lldb
+      taplo # for editing toml
+      convco # commit message linter
+      rustPackages.clippy # rust linter
+      rust-analyzer # rust language server
+      rustfmt # rust formatter
+      nixd # nix language server
+      lld # wasm linker
+      lldb # debugger
     ];
   };
 }
