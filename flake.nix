01575193878c2c05f8643800e397d4ea3e4cb6ae
Daniel Flanagan <daniel@lyte.dev>
2025-04-19 22:04:22 -0500

    feat: shallow inputs

--- flake.nix
+++ flake.nix
@@ -37,59 +37,59 @@
 
   inputs = {
     # stable inputs
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11?shallow=1";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-24.11?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # unstable inputs
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable?shallow=1";
 
     home-manager-unstable = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     disko = {
-      url = "github:nix-community/disko/master";
+      url = "github:nix-community/disko/master?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     sops-nix = {
-      url = "github:Mic92/sops-nix";
+      url = "github:Mic92/sops-nix?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     git-hooks = {
-      url = "github:cachix/git-hooks.nix";
+      url = "github:cachix/git-hooks.nix?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     slippi = {
-      url = "github:lytedev/slippi-nix";
+      url = "github:lytedev/slippi-nix?shallow=1";
       # url = "git+file:///home/daniel/code/open-source/slippi-nix"; # used during flake development
       inputs.nixpkgs.follows = "nixpkgs-unstable";
       inputs.home-manager.follows = "home-manager-unstable";
     };
 
-    jovian.url = "github:Jovian-Experiments/Jovian-NixOS/development";
+    jovian.url = "github:Jovian-Experiments/Jovian-NixOS/development?shallow=1";
     # jovian.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     # other inputs
-    hardware.url = "github:NixOS/nixos-hardware";
+    hardware.url = "github:NixOS/nixos-hardware?shallow=1";
     # nnf.url = "github:thelegy/nixos-nftables-firewall";
 
     # inputs with their own cache I want to use
-    helix.url = "github:helix-editor/helix/master";
+    helix.url = "github:helix-editor/helix/master?shallow=1";
     # helix.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
-    ghostty.url = "github:ghostty-org/ghostty";
+    ghostty.url = "github:ghostty-org/ghostty?shallow=1";
     # ghostty.inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
     # ghostty.inputs.nixpkgs-stable.follows = "nixpkgs";
 
-    deploy-rs.url = "github:serokell/deploy-rs";
+    deploy-rs.url = "github:serokell/deploy-rs?shallow=1";
     # deploy-rs.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
