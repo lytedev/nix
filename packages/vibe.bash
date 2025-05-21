0f613293ff49d153e005a4c834d28dedaf48b7ed
Daniel Flanagan <daniel@lyte.dev>
2025-05-21 09:43:35 -0500

    fix: default vibe model

--- packages/vibe.bash
+++ packages/vibe.bash
@@ -16,7 +16,7 @@ echo "Additional aider args: $*"
 
 # MODEL="${MODEL:-gemini/gemini-2.0-flash}"
 # MODEL="bedrock/anthropic.claude-3-7-sonnet-20250219-v1:0"
-MODEL="${MODEL:-"gemini-exp"}"
+MODEL="${MODEL:-"gemini/gemini-2.5-pro-preview-03-25"}"
 
 # avoid using the nixpkgs registry entry, we want the latest hotness from unstable
 AIDER_DARK_MODE=true nix shell "github:nixos/nixpkgs?rev=648a999c9cebef4b2ab474f4a85dd3679309bd28#aider-chat-full" -c aider --model "$MODEL" "$@"
