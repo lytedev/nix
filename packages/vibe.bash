468b6e801ebb826d500f9c711120ee2c25e0988c
Daniel Flanagan <daniel@lyte.dev>
2025-06-04 10:03:55 -0500

    fix: notify user about default model changes based on context

--- packages/vibe.bash
+++ packages/vibe.bash
@@ -12,20 +12,21 @@ pip install -U google-generativeai boto3
 GEMINI_API_KEY="$(comma yq .clients[0].api_key "$HOME/.config/aichat/config.yaml")"
 export GEMINI_API_KEY
 
-if [[ ! "$*" =~ "--watch-files" ]]; then
+if [[ ! $* =~ --watch-files ]]; then
   echo 'TIP: Add "--watch-files" to interact with changes in your EDITOR.'
 fi
 
 DEFAULT_MODEL="gemini/gemini-2.5-pro-preview-03-25"
 
-if [[ "$(pwd)" == */bill/* ]]; then
-  DEFAULT_MODEL="billmodel"
+if [[ $PWD =~ bill ]]; then
+  echo "In a \$JOB project. You may want to ensure AWS_PROFILE is set properly!"
+  DEFAULT_MODEL="bedrock/anthropic.claude-sonnet-4-20250514-v1:0"
 fi
 
 # MODEL="${MODEL:-gemini/gemini-2.0-flash}"
 # MODEL="bedrock/anthropic.claude-3-7-sonnet-20250219-v1:0"
 MODEL="${MODEL:-$DEFAULT_MODEL}"
-
+echo "Using MODEL=${MODEL}"
 
 # avoid using the nixpkgs registry entry, we want the latest hotness from unstable
 AIDER_DARK_MODE=true nix shell "github:nixos/nixpkgs?rev=648a999c9cebef4b2ab474f4a85dd3679309bd28#aider-chat-full" -c aider --model "$MODEL" "$@"
