774ad4e83da6701c995e466a518097effc7eb23a
Daniel Flanagan <daniel@lyte.dev>
2025-11-12 09:34:58 -0600

    feat: more noctalia tinkering, make daniel a postgres superuser

--- lib/modules/nixos/postgres.nix
+++ lib/modules/nixos/postgres.nix
@@ -14,6 +14,11 @@
         {
           name = "daniel";
           ensureDBOwnership = true;
+          ensureClauses = {
+            superuser = true;
+            createrole = true;
+            createdb = true;
+          };
         }
       ];
       # enableTCPIP = true;
