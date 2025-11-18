d4dc930dd40fe6ffd5a60e91a458ee609d57a196
Daniel Flanagan <daniel@lyte.dev>
2025-11-17 23:04:51 -0600

    chore: move common steamdeck config to nixos module

---
+++ lib/modules/nixos/steamdeck.nix
@@ -0,0 +1,80 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  options.lyte.steamdeck = {
+    enable = lib.mkEnableOption "Steam Deck configuration";
+  };
+
+  config = lib.mkIf config.lyte.steamdeck.enable {
+    hardware.bluetooth.enable = true;
+    boot = {
+      # kernelPackages = pkgs.linuxPackages_latest; # do NOT use with jovian config
+      loader = {
+        efi.canTouchEfiVariables = true;
+        systemd-boot.enable = true;
+      };
+    };
+
+    lyte.desktop.enable = true;
+    lyte.shell.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      steamdeck-firmware
+      steam-rom-manager
+    ];
+
+    services.flatpak.enable = true;
+    systemd.services.flatpak-repo = {
+      wantedBy = [ "multi-user.target" ];
+      path = [ pkgs.flatpak ];
+      script = ''
+        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      '';
+    };
+
+    services.displayManager.gdm.enable = lib.mkForce false;
+
+    home-manager.users.daniel = {
+      lyte = {
+        useOutOfStoreSymlinks.enable = true;
+        shell = {
+          enable = true;
+          learn-jujutsu-not-git.enable = true;
+        };
+        desktop.enable = true;
+      };
+    };
+
+    nixpkgs.config.allowUnfree = true;
+    programs.steam.enable = true;
+
+    jovian = {
+      decky-loader = {
+        enable = true;
+      };
+      steam = {
+        enable = true;
+        autoStart = true;
+        desktopSession = "gnome";
+        user = "daniel";
+        updater = {
+          splash = "jovian";
+        };
+      };
+      hardware = {
+        has.amd.gpu = true;
+      };
+      devices = {
+        steamdeck = {
+          enable = true;
+          autoUpdate = true;
+          enableGyroDsuService = true;
+        };
+      };
+    };
+  };
+}
--- packages/hosts/steamdeckoled.nix
+++ packages/hosts/steamdeckoled.nix
@@ -1,7 +1,5 @@
 {
   diskoConfigurations,
-  pkgs,
-  lib,
   # hardware, # do NOT use nixos-hardware with jovian config
   ...
 }:
@@ -13,69 +11,9 @@
     wifi.enable = true;
   };
 
-  hardware.bluetooth.enable = true;
-  boot = {
-    # kernelPackages = pkgs.linuxPackages_latest; # do NOT use with jovian config
-    loader = {
-      efi.canTouchEfiVariables = true;
-      systemd-boot.enable = true;
-    };
-  };
   imports = [
     (diskoConfigurations.unencrypted { disk = "/dev/nvme0n1"; })
   ];
 
-  lyte.desktop.enable = true;
-  lyte.shell.enable = true;
-  environment.systemPackages = with pkgs; [
-    steamdeck-firmware
-    steam-rom-manager
-  ];
-  services.flatpak.enable = true;
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
-
-  services.displayManager.gdm.enable = lib.mkForce false;
-  home-manager.users.daniel = {
-    lyte = {
-      useOutOfStoreSymlinks.enable = true;
-      shell = {
-        enable = true;
-        learn-jujutsu-not-git.enable = true;
-      };
-      desktop.enable = true;
-    };
-  };
-
-  nixpkgs.config.allowUnfree = true;
-  programs.steam.enable = true;
-  jovian = {
-    decky-loader = {
-      enable = true;
-    };
-    steam = {
-      enable = true;
-      autoStart = true;
-      desktopSession = "gnome";
-      user = "daniel";
-      updater = {
-        splash = "jovian";
-      };
-    };
-    hardware = {
-      has.amd.gpu = true;
-    };
-    devices = {
-      steamdeck = {
-        enable = true;
-        autoUpdate = true;
-        enableGyroDsuService = true;
-      };
-    };
-  };
+  lyte.steamdeck.enable = true;
 }
