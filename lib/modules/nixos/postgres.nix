bc092ca4fd52d79a79c05f5dae244651b3f0fbb9
Daniel Flanagan <daniel@lyte.dev>
2025-11-19 14:18:59 -0600

    fix: postgres local connections over ipv6

--- lib/modules/nixos/postgres.nix
+++ lib/modules/nixos/postgres.nix
@@ -23,29 +23,33 @@
       ];
       # enableTCPIP = true;
 
-      package = pkgs.postgresql_15;
+      package = pkgs.postgresql_17;
 
       authentication = pkgs.lib.mkOverride 10 ''
         #type database  DBuser    auth-method
-        local all       postgres  peer map=superuser_map
-        local all       daniel    peer map=superuser_map
-        local sameuser  all       peer map=superuser_map
+        local all       all       peer map=superuser_map
+        local sameuser  all       peer map=user_map
 
         # lan ipv4
         host  all       all     10.0.0.0/24   trust
         host  all       all     127.0.0.1/32  trust
+        host  all       all     ::1/128       trust
 
         # tailnet ipv4
         host       all       all     100.64.0.0/10 trust
       '';
 
       identMap = ''
-        # ArbitraryMapName systemUser DBUser
-        superuser_map      root       postgres
+        # mapName          linuxUser  postgresUser
         superuser_map      postgres   postgres
+        superuser_map      root       postgres
         superuser_map      daniel     postgres
+        superuser_map      root       root
+        superuser_map      daniel     daniel
 
-        superuser_map      /^(.*)$    \1       # Let other names login as themselves
+        superuser_map      daniel     all
+
+        user_map           /^(.*)$/   \1           # Let other names login as themselves
       '';
     };
 
