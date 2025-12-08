64ca53fad1148e47dc9166bb4dc022210078f01b
Daniel Flanagan <daniel@lyte.dev>
2025-12-08 00:09:28 -0600

    fix: steamdeck flatpak setup requires network

--- lib/modules/nixos/steamdeck.nix
+++ lib/modules/nixos/steamdeck.nix
@@ -30,10 +30,18 @@
     services.flatpak.enable = true;
     systemd.services.flatpak-repo = {
       wantedBy = [ "multi-user.target" ];
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
       path = [ pkgs.flatpak ];
       script = ''
         flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
       '';
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        Restart = "on-failure";
+        RestartSec = "5s";
+      };
     };
 
     services.displayManager.gdm.enable = lib.mkForce false;
