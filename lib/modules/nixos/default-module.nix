bab3bee272b2eddfc99508bb7f85a0a4b6fadbbf
Daniel Flanagan <daniel@lyte.dev>
2026-02-20 23:42:51 -0600

    refactor: remove redundant /etc/lytedev files, use configurationRevision

--- lib/modules/nixos/default-module.nix
+++ lib/modules/nixos/default-module.nix
@@ -118,12 +118,6 @@
     system.configurationRevision = toString (
       self.shortRev or self.dirtyShortRev or self.lastModified or "unknown"
     );
-    environment = {
-      etc = {
-        "lytedev/rev".text = config.system.configurationRevision;
-        "lytedev/lastModified".text = toString (self.lastModified or "unknown");
-      };
-    };
 
     lyte.shell.enable = lib.mkDefault true;
     nixpkgs = {
