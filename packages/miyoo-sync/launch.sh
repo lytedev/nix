58d3a07fc9aea85a0fab739288934b4d5d0d5bb5
Daniel Flanagan <daniel@lyte.dev>
2026-03-18 15:16:12 -0500

    chore: rename app to Sync ROMs & Saves

--- packages/miyoo-sync/launch.sh
+++ packages/miyoo-sync/launch.sh
@@ -6,7 +6,7 @@ APPDIR="$(dirname "$0")"
 cd "$APPDIR"
 
 touch /tmp/stay_awake
-infoPanel --title "ROM Sync" --message "Syncing..." --auto &
+infoPanel --title "Sync ROMs & Saves" --message "Syncing..." --auto &
 PANEL_PID=$!
 
 sh ./sync.sh > ./sync.log 2>&1
@@ -15,9 +15,9 @@ RC=$?
 kill $PANEL_PID 2>/dev/null
 
 if [ $RC -eq 0 ]; then
-    infoPanel --title "ROM Sync" --message "Sync complete!\n\nPress any button to exit."
+    infoPanel --title "Sync ROMs & Saves" --message "Sync complete!\n\nPress any button to exit."
 else
-    infoPanel --title "ROM Sync" --message "Sync had errors, check sync.log\n\nPress any button to exit."
+    infoPanel --title "Sync ROMs & Saves" --message "Sync had errors, check sync.log\n\nPress any button to exit."
 fi
 
 rm -f /tmp/stay_awake
