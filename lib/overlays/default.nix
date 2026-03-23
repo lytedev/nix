203c849370b68a351448c622d40f4675b01a3d95
Daniel Flanagan <daniel@lyte.dev>
2026-03-23 10:52:09 -0500

    style: format overlay nix after atuin changes

--- lib/overlays/default.nix
+++ lib/overlays/default.nix
@@ -39,76 +39,76 @@ let
           };
         in
         unstable-packages.callPackage (
-        {
-          fetchFromGitHub,
-          installShellFiles,
-          lib,
-          stdenv,
-          nixosTests,
-          nix-update-script,
-        }:
-        rustPlatform'.buildRustPackage (finalAttrs: {
-          pname = "atuin";
-          version = "18.13.3";
+          {
+            fetchFromGitHub,
+            installShellFiles,
+            lib,
+            stdenv,
+            nixosTests,
+            nix-update-script,
+          }:
+          rustPlatform'.buildRustPackage (finalAttrs: {
+            pname = "atuin";
+            version = "18.13.3";
 
-          src = fetchFromGitHub {
-            owner = "atuinsh";
-            repo = "atuin";
-            tag = "v${finalAttrs.version}";
-            hash = "sha256-hLt6CDHEPV8BVpOADVn4bLNcBz89eC2jKtIexHG0yAY=";
-          };
-
-          cargoHash = "sha256-VYwzMnfc/a4Sghmr5oMfhvoMkaWlY4w4e4Flu8MWQg0=";
-
-          buildNoDefaultFeatures = true;
-          buildFeatures = [
-            "client"
-            "sync"
-            "clipboard"
-            "daemon"
-          ];
-
-          nativeBuildInputs = [ installShellFiles ];
-
-          postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-            installShellCompletion --cmd atuin \
-              --bash <($out/bin/atuin gen-completions -s bash) \
-              --fish <($out/bin/atuin gen-completions -s fish) \
-              --zsh <($out/bin/atuin gen-completions -s zsh)
-          '';
-
-          checkFlags = [
-            "--skip=registration"
-            "--skip=sync"
-            "--skip=change_password"
-            "--skip=multi_user_test"
-          ];
-
-          preCheck = ''
-            export HOME=$(mktemp -d)
-          '';
-
-          passthru = {
-            tests = {
-              inherit (nixosTests) atuin;
+            src = fetchFromGitHub {
+              owner = "atuinsh";
+              repo = "atuin";
+              tag = "v${finalAttrs.version}";
+              hash = "sha256-hLt6CDHEPV8BVpOADVn4bLNcBz89eC2jKtIexHG0yAY=";
             };
-            updateScript = nix-update-script { };
-          };
 
-          meta = {
-            description = "Replacement for a shell history which records additional commands context with optional encrypted synchronization between machines";
-            homepage = "https://github.com/atuinsh/atuin";
-            license = lib.licenses.mit;
-            maintainers = with lib.maintainers; [
-              SuperSandro2000
-              sciencentistguy
-              _0x4A6F
-              rvdp
+            cargoHash = "sha256-VYwzMnfc/a4Sghmr5oMfhvoMkaWlY4w4e4Flu8MWQg0=";
+
+            buildNoDefaultFeatures = true;
+            buildFeatures = [
+              "client"
+              "sync"
+              "clipboard"
+              "daemon"
             ];
-            mainProgram = "atuin";
-          };
-        })
-      ) { };
+
+            nativeBuildInputs = [ installShellFiles ];
+
+            postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+              installShellCompletion --cmd atuin \
+                --bash <($out/bin/atuin gen-completions -s bash) \
+                --fish <($out/bin/atuin gen-completions -s fish) \
+                --zsh <($out/bin/atuin gen-completions -s zsh)
+            '';
+
+            checkFlags = [
+              "--skip=registration"
+              "--skip=sync"
+              "--skip=change_password"
+              "--skip=multi_user_test"
+            ];
+
+            preCheck = ''
+              export HOME=$(mktemp -d)
+            '';
+
+            passthru = {
+              tests = {
+                inherit (nixosTests) atuin;
+              };
+              updateScript = nix-update-script { };
+            };
+
+            meta = {
+              description = "Replacement for a shell history which records additional commands context with optional encrypted synchronization between machines";
+              homepage = "https://github.com/atuinsh/atuin";
+              license = lib.licenses.mit;
+              maintainers = with lib.maintainers; [
+                SuperSandro2000
+                sciencentistguy
+                _0x4A6F
+                rvdp
+              ];
+              mainProgram = "atuin";
+            };
+          })
+        ) { };
 
       # bleeding-edge Firefox variants
       firefox-nightly = firefox-nightly.packages.${prev.system}.firefox-nightly-bin;
