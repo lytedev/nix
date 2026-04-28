3d6045c0678d0e0b73dc9faec08ea97378fd5733
Daniel Flanagan <daniel@lyte.dev>
2026-04-28 14:52:11 -0500

    feat(beefcake): add hearth home dashboard service
    - New hearth.nix module: podman container on port 8473, caddy virtual host, hearth user/group, /var/lib/hearth tmpfiles
    - beefcake.nix: import hearth module, add hearth.h to dns-updater records
    - Image (localhost/hearth:latest) and secrets (/var/lib/hearth/hearth.env) are deployed by the hearth-leptos deploy script, not managed by nix
    - headscale-server.nix: add --accept-dns so Magic DNS hostnames resolve on all tailnet clients

--- packages/hosts/beefcake/hearth.nix
+++ packages/hosts/beefcake/hearth.nix
@@ -17,12 +17,12 @@
   virtualisation.oci-containers.containers.hearth = {
     image = "localhost/hearth:latest";
     autoStart = true;
-    ports = [ "127.0.0.1:3000:3000" ];
+    ports = [ "127.0.0.1:8473:8473" ];
     # hearth.env is pushed to /var/lib/hearth/hearth.env by the deploy script
     environmentFiles = [ "/var/lib/hearth/hearth.env" ];
     environment = {
       LEPTOS_SITE_ROOT = "/app/site";
-      LEPTOS_SITE_ADDR = "0.0.0.0:3000";
+      LEPTOS_SITE_ADDR = "0.0.0.0:8473";
       LEPTOS_OUTPUT_NAME = "hearth";
       LEPTOS_SITE_PKG_DIR = "pkg";
     };
@@ -30,7 +30,7 @@
 
   services.caddy.virtualHosts."hearth.h.lyte.dev" = {
     extraConfig = ''
-      reverse_proxy :3000
+      reverse_proxy :8473
     '';
   };
 }
